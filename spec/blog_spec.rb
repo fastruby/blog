7ed8633bed0e116e25c6bb71765b5266341b427d
Cleiviane Costa <costa.cleiviane@gmail.com>
2019-02-14 18:01:27 -0200

    Fixing rspec tests

--- spec/blog_spec.rb
+++ spec/blog_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Fast Ruby Blog' do
   describe 'tests' do
     let(:post) { '/rails/upgrades/case-study/upgrading-a-monolith.html' }
     let(:file_path) { "_site/#{post}" }
-    let(:url) { "https://fastruby.io/blog/#{post}" }
+    let(:url) { "https://fastruby.io/blog#{post}" }
 
     before do
       unless File.exists?(file_path)
@@ -33,7 +33,7 @@ RSpec.describe 'Fast Ruby Blog' do
     it 'generates the sitemap with correct domain name' do
       doc = Nokogiri::XML(IO.read('_site/sitemap.xml'))
       url = doc.xpath('/aws:urlset/aws:url/aws:loc', 'aws' => 'http://www.sitemaps.org/schemas/sitemap/0.9')[0]
-      expect(url.text).to eq 'https://fastruby.io//blog'
+      expect(url.text).to eq 'https://fastruby.io/blog'
     end
 
     it 'generates share button for twitter' do
