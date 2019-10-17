593477be55064250931df961618c1490b3f585b3
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2019-10-17 17:28:49 -0400

    Consistent quotes

--- _plugins/sitemap.rb
+++ _plugins/sitemap.rb
@@ -1,20 +1,20 @@
-require 'sitemap_generator'
+require "sitemap_generator"
 
 Jekyll::Hooks.register :site, :post_write do |site|
   unless site.config["watch"]
     puts "Generating sitemap.xml.gz"
 
     files = []
-    Dir['_site/**/*.html'].each do |page|
+    Dir["_site/**/*.html"].each do |page|
       files << File.new(page)
     end
 
-    SitemapGenerator::Sitemap.default_host = site.config['url'] + "/blog"
+    SitemapGenerator::Sitemap.default_host = site.config["url"] + "/blog"
     public_path = Dir.pwd.end_with?("blog") ? "_site" : "public/blog"
     SitemapGenerator::Sitemap.public_path = public_path
     SitemapGenerator::Sitemap.create compress: false do
       files.each do |file|
-        add file.path.sub(/^_site/,''), changefreq: 'weekly'
+        add file.path.sub(/^_site/,""), changefreq: "weekly"
       end
     end
 
